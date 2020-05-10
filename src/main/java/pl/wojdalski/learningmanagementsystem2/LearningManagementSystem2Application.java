bae306922e050e5144c977a11ee8b979ffb94b88
Wojdalski Mateusz <wojdalski.m@gmail.com>
2020-05-10 20:52:01 +0200

    Initial commit

---
+++ src/main/java/pl/wojdalski/learningmanagementsystem2/LearningManagementSystem2Application.java
@@ -0,0 +1,13 @@
+package pl.wojdalski.learningmanagementsystem2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningManagementSystem2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningManagementSystem2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wojdalski/learningmanagementsystem2/LearningManagementSystem2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojdalski.learningmanagementsystem2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningManagementSystem2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
